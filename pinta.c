acdbe30b376354db9e4670a0c6cd4dd2b1a37519
Angelo <angelocristobalep@gmail.com>
2018-11-07 14:44:45 -0300

    Fix error pinta.c

--- pinta.c
+++ pinta.c
@@ -5,9 +5,20 @@
 int main(){
 	int gdriver=DETECT,gmode;
 	initgraph(&gdriver,&gmode,"");
+
 	setcolor(2);
-	line(0,0,10,7);
+	line(0,0,100,70);
 	getch();
+	void wait_for_char()
+	{
+
+    //Wait for a key press
+    int in = 0;
+
+		while (in == 0) {
+			in = getchar();
+		}
+	}
 	closegraph();
 
 	return 0;
